806e8e40c56c99fd634aae84aba823083cf912ac
Alexander Shiryshev <shiryshev@gmail.com>
2026-07-03 02:41:38 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Lala Bahasa service worker — кэш для офлайна и установки иконки.
 // Версию меняем при обновлении базы, чтобы телефон подхватил новые слова.
-var CACHE = "lala-bahasa-v10";
+var CACHE = "lala-bahasa-v11";
 var ASSETS = [
   "./",
   "./index.html",
